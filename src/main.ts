fab18b877c635cd8ec4f54eb1cccd274c5ed02cc
Fernando Diaz <f.diazlaclaustra@umlaud.es>
2018-10-17 00:29:00 +0200

    Mueve main.ts fuera de /src/app, a la raiz de /src.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { Pinakes } from './pinakes.module';
+import { Pinakes } from './app/pinakes.module';
 
 
 platformBrowserDynamic().bootstrapModule(Pinakes);
