f9d1f9177e5be95adc884bf63e84cfa7ba6348a1
Fernando Diaz <f.diazlaclaustra@umlaud.es>
2018-11-10 18:01:34 +0100

    Sustituye declaraciones de componentes individuales por el generico app.

--- src/app/pinakes.module.ts
+++ src/app/pinakes.module.ts
@@ -2,48 +2,33 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { RouteReuseStrategy } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { IonicModule, IonicErrorHandler, IonicRouteStrategy } from '@ionic/angular';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
-import { AppRoutingModule } from './app-routing.module';
 import { App } from './app.component';
-
-import { HomePage } from './pages/home/home.component';
-import { TabsPage } from './pages/tabs/tabs.component';
-import { LibraryPage } from './pages/library/library.component';
-import { BookComponent } from './pages/library/book.component';
-import { AboutPage } from './pages/about/about.component';
-import { ContactPage } from './pages/contact/contact.component';
-
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { BookService } from './services/book.service';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
 
 @NgModule({
   imports: [
+    BrowserModule
     IonicModule.forRoot(),
     AppRoutingModule,
-    BrowserModule
   ],
   declarations: [
     App,
-    TabsPage,
-    HomePage,
-    LibraryPage,
-    BookComponent,
-    ContactPage,
-    AboutPage,
   ],
-  bootstrap: [App],
+  bootstrap: [
+    App
+  ],
   entryComponents: [],
   providers: [
     StatusBar,
     SplashScreen,
-    BookService,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: ErrorHandler, useClass: ErrorHandler},
     {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
   ]
 })
